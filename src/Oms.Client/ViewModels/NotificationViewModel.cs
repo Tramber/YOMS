0905ad227e403d419f5787046a749b5fa4b6bb17
Tramber <tramber@gmail.com>
2015-03-15 22:34:51 +0100

    Notification view enhancement

---
+++ src/Oms.Client/ViewModels/NotificationViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Caliburn.Micro;
+using Oms.Client.Services.Notifications;
+
+namespace Oms.Client.ViewModels
+{
+    [Export(typeof(INotificationViewModel))]
+    public class NotificationViewModel : Screen, INotificationViewModel
+    {
+        public NotificationViewModel(INotification notification)
+        {
+            Notification = notification;
+        }
+
+        public INotification Notification { get; private set; }
+
+    }
+}
